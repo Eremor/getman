4e4ccfc08b929a3aceea3c825f42814ff877ae4d
Eremor <artem.s13@gmail.com>
2019-02-18 16:52:03 +0300

    fixed svg icons + add slider

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,18 @@
 var slideNow = 1;
-var slideCount = $('.slider__area').children().length);
+var slideCount = 3 //number of slides
 var translateWidth = 0;
-var slideInterval = 2000;
+var slideInterval = 10000;
 
 $(document).ready(function () {
-    setInterval(nextSlide, slideInterval);
+  setInterval(nextSlide, slideInterval);
 
-    $('.slider__arrow--right').click(function() {
-        nextSlide();
-    });
+  $('.slider__arrow--right').click(function() {
+      nextSlide();
+  });
+
+  $('.slider__arrow--left').click(function() {
+      prevSlide();
+  });
 });
 
 function nextSlide() {
@@ -24,4 +28,24 @@ function nextSlide() {
     });
     slideNow++;
   }
+}
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('.slider__inner').width() * (slideCount - 1);
+        $('.slider__area').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('.slider__inner').width() * (slideNow - 2);
+        $('.slider__area').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
 }
