71bb33b6cca14810a9b7b35bbd0bef2257faa5ab
Eremor <artem.s13@gmail.com>
2019-02-19 22:33:32 +0300

    add main-nav__item--desktop class

--- js/header-effects.js
+++ js/header-effects.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
-  $('body').on('click', '.main-nav__descktop .main-nav__item',
-  function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
-    event.preventDefault();
-  });
-
   $(window).on("scroll", function() {
     if($(window).scrollTop() > 50) {
         $(".page-header").addClass("active");
     } else {
        $(".page-header").removeClass("active");
     }
-});
+  });
 });
